fcd024a20f70d6abf5ef975a57a7ffca6334cf12
Vinh Huynh <huynhtanvinhktqn123@gmail.com>
2022-06-24 10:33:31 +0700

    update code 5

---
+++ src/Component/Login_Register/Login_Register.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import './Login_Register.css'
+import Login from './Login/Login'
+import Register from './Register/Register'
+import ForgetPassword from './ForgetPassword/ForgetPassword'
+function LOGIN_REGISTER(props) {
+  const [option, setOption] = useState(1)
+
+  const getOption = (data, data2) => {
+    props.callBack(data2)
+    setOption(data)
+  }
+
+  return (
+    <div className='LOGIN_REGISTER'>
+      {option === 1 && <Login callBack={getOption} />}
+      {option === 2 && <Register callBack={getOption} />}
+      {option === 3 && <ForgetPassword callBack={getOption} />}
+      <div className='overlay' />
+    </div>
+  )
+}
+
+export default LOGIN_REGISTER
