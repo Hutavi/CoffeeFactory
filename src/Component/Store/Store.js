6cf5c138c893bf4a62d930ffd608f6c414e24913
Vinh Huynh <huynhtanvinhktqn123@gmail.com>
2022-07-06 09:49:58 +0700

    update react router and introducePage

--- src/Component/Store/Store.js
+++ src/Component/Store/Store.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Navbar from '../Home/Header/Navbar/Navbar';
+import "./Store.css"
 
 
 const Store = () => {
     return (
         <div>
-            <p>Khương idol</p>
+            <Navbar/>
         </div>
     );
 };
